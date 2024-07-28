892dc058646432589087b7f57448575607ce679d
Sai <Sai-RWC@github.com>
2024-07-29 01:54:49 +0800

    working binomial heap insert runtime test

--- src/driver.java
+++ src/driver.java
@@ -9,18 +9,45 @@ import src.*;
 public class driver {
   public static void main(String[] args) {
 
+    // 0 => Binary Heap
+    // 1 => Binomial Heap
+    // 2 => Fibonacci Heap
+    int operation = 1;
+
     BinaryHeap heap = new BinaryHeap();
 
-    ReadFile file = new ReadFile("./data/random75000_num.txt"); 
-    file.printIn();
+    ReadFile file = new ReadFile("./data/random100000_num.txt"); 
+    // file.printIn();
 
-    
-    long startTime = System.currentTimeMillis();
-    for (int i = 0; i < file.arrIn.size(); i++) {
-      heap.insert(file.arrIn.get(i));
+    BinomialHeap heap_binom = new BinomialHeap();
+
+    long startTime = System.nanoTime();
+    switch (operation) {
+      case 0:
+        // BinaryHeap Runtime
+
+        // long startTime = System.currentTimeMillis();
+        for (int i = 0; i < file.arrIn.size(); i++) {
+          heap.insert(file.arrIn.get(i));
+        }
+        break;
+      case 1:
+
+        // BinomialHeap Runtime
+        for (int i = 0; i < file.arrIn.size(); i++) {
+          heap_binom.insert(file.arrIn.get(i));
+        }
+        // long endTime = System.currentTimeMillis();
+        break;
+
+      default:
+        break;
     }
-    long endTime = System.currentTimeMillis();
+    long endTime = System.nanoTime();
+
+
     long duration = endTime - startTime;
-    System.out.println("Insert operation took "+duration+"ms");
+
+    System.out.println("Insert operation took "+duration+"ns");
   }
 }
